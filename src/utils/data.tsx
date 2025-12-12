035802b7d4d4fdc161b17a32d86d6375a20e1cbc
Tibesti <tobilobaolugbemi@gmail.com>
2025-12-12 01:03:12 +0100

    moved data to new file

---
+++ src/utils/data.tsx
@@ -0,0 +1,255 @@
+
+// Client Stories Images
+import unityStory from '../assets/images/655f17af86fbbf271ff8a74b_Unity.jpg'
+import coinbaseStory from '../assets/images/655f18081018f7cc675e8e12_Coinbase.jpg'
+import statsPerformStory from '../assets/images/655f18571018f7cc675eb8ed_Stats_Perform.jpg'
+import atlasCopcoStory from '../assets/images/655f18a6878c50e34216250a_Atlas_Copco.jpg'
+import burdaStory from '../assets/images/655f18f4e1f5554d59c763a1_Burda.jpg'
+import ltseStory from '../assets/images/655f0c2d769318df75d0f64b_LTSE.jpg'
+
+// Numbers GIFs
+import itekako150Employees from '../assets/gif/650002473f676cb651d756f7_Itekako_150_Employees.gif'
+import itekako15Years from '../assets/gif/650002419c1d7e87b816e868_Itekako_15_Years.gif'
+import itekako200Projects from '../assets/gif/650002461d7831d93c5900c9_Itekako_200_Projects.gif'
+import itekako90Clients from '../assets/gif/65000246b3cac0e307d2068f_Itekako_90_Clients.gif'
+
+// Service Icons
+import customSoftwareIcon from '../assets/svg/6519742dfa2015e8d622f7b3_Custom_Software.svg'
+import frontendBackendIcon from '../assets/svg/6516a1e53662e327a774b6b1_Front-end_and_back-end_-_Icon.svg'
+import aiMachineLearningIcon from '../assets/svg/6516a1e5553129e72121f6eb_AI_and_Machine_learning_Icon.svg'
+import iotEmbeddedIcon from '../assets/svg/6516a1e5a368e1dec420d2dc_IoT_and_Embedded_software_Icon.svg'
+import realtimeCommunicationIcon from '../assets/svg/6516a1e55a394102aef8ca77_Real-time_communication_Icon.svg'
+import videoStreamingIcon from '../assets/svg/6516a1e5ff59206f48ab03ec_Video-streaming_solutions_Icon.svg'
+import productManagementIcon from '../assets/svg/6516a1e54e3fb42aa1991f04_Product_management_Icon.svg'
+import devopsMigrationsIcon from '../assets/svg/6519742d4f90a63cf7335dff_DevOps_and_Migrations.svg'
+import assessmentIcon from '../assets/svg/6519def584290dd6869f35d7_Assessment.svg'
+import automationIcon from '../assets/svg/6519def584290dd6869f362a_Automation.svg'
+import managementIcon from '../assets/svg/6519def5b575d1272cc4043b_Management.svg'
+import migrationsIcon from '../assets/svg/6519def69b6ee8bb7255eebd_Migrations.svg'
+import managedServicesIcon from '../assets/svg/6519def53ae7212cf5d6ccfb_Managed_services.svg'
+import cyberSecurityIcon from '../assets/svg/6519def4cdf8f5f54c6310fd_Cyber_security.svg'
+import softwareTestingQAIcon from '../assets/svg/6519742dc9eeec6fbac9cad5_Software_testing_and_QA.svg'
+import automatedTestingIcon from '../assets/svg/6519df93538e0b47baf1b554_Automated_testing.svg'
+import manualTestingIcon from '../assets/svg/6519df934e1cbb1f61bd9b2d_Manual_testing.svg'
+import performanceTestingIcon from '../assets/svg/6551f7cca635c170acc63d8b_Performance_Testing_Icon.svg'
+import consultingIcon from '../assets/svg/6519742dc9eeec6fbac9cacb_Consulting.svg'
+import projectSpecDevIcon from '../assets/svg/6519e06a84290dd686a0848a_Project_specification_development.svg'
+import digitalTransformationIcon from '../assets/svg/6519dfee17cd8731e21b9cb1_Digital_transformation.svg'
+import technicalDueDiligenceIcon from '../assets/svg/6519dfec437a1b792528cf2d_Technical_due_diligence.svg'
+import buildingDigitalProductsIcon from '../assets/svg/6519742e09b135a52cd4135f_Building_digital_products_end-to-end.svg'
+import conceptualizationPrototypingIcon from '../assets/svg/6519e0aaf5bb2df8dd5b9efa_Conceptualization___Prototyping.svg'
+import productSolutionDesignIcon from '../assets/svg/6519dfee1d65ace28423dee3_Product___Solution_design.svg'
+import mvpDevelopmentIcon from '../assets/svg/6519e0a9ba01c1e2342f7004_MVP_development.svg'
+import softwareDevelopmentIcon from '../assets/svg/6519e0a9437a1b79252959b5_Software_development.svg'
+import softwareDeploymentIcon from '../assets/svg/6519e0a94e1cbb1f61bea1dd_Software_deployment.svg'
+import maintenanceIcon from '../assets/svg/6519e0a9b2d820de93ee1b39_Maintenance.svg'
+
+// Work Mode Icons
+import partnership from '../assets/svg/partnership.svg'
+import team from '../assets/svg/team.svg'
+import project from '../assets/svg/project.svg'
+
+export const CLIENT_STORIES = [
+  {
+    image: unityStory,
+    title: 'Internal software tools for support of internal development process',
+  },
+  {
+    image: coinbaseStory,
+    title: 'Ensuring Security and Reliability Across Cryptocurrency Applications',
+  },
+  {
+    image: statsPerformStory,
+    title: 'Who needs excitement that sport brings when you can turn it into numbers',
+  },
+  {
+    image: atlasCopcoStory,
+    title: 'Heavy duty software for industrial equipment for reliable manufacturing',
+  },
+  {
+    image: burdaStory,
+    title: 'Transforming a legacy platform for a seamless fashion community experience',
+  },
+  {
+    image: ltseStory,
+    title: 'Building a successful B2B platform from scratch to support rapid growth',
+  },
+]
+
+export const NUMBERS = [
+  {
+    image: itekako150Employees,
+    label: '15+ Years',
+  },
+  {
+    image: itekako15Years,
+    label: '150+ Employees',
+  },
+  {
+    image: itekako200Projects,
+    label: '200+ Projects',
+  },
+  {
+    image: itekako90Clients,
+    label: '90+ Clients',
+  },
+]
+
+export const SERVICES = [
+  {
+    name: 'Custom software development',
+    moreLink: 'https://www.itekako.com/custom-software-development',
+    icon: customSoftwareIcon,
+    items: [
+      ['Front-end and back-end development', frontendBackendIcon],
+      ['AI and Machine learning', aiMachineLearningIcon],
+      ['IoT and Embedded software', iotEmbeddedIcon],
+      ['Real-time communication', realtimeCommunicationIcon],
+      ['Video-streaming solutions', videoStreamingIcon],
+      ['Product management', productManagementIcon],
+    ],
+  },
+  {
+    name: 'DevOps and Migrations',
+    moreLink: 'https://www.itekako.com/devops-and-migrations',
+    icon: devopsMigrationsIcon,
+    items: [
+      ['Assessment', assessmentIcon],
+      ['Automation', automationIcon],
+      ['Management', managementIcon],
+      ['Migrations', migrationsIcon],
+      ['Managed services', managedServicesIcon],
+      ['Cyber security, penetration testing', cyberSecurityIcon],
+    ],
+  },
+  {
+    name: 'Software testing and QA',
+    moreLink: 'https://www.itekako.com/software-testing-and-qa',
+    icon: softwareTestingQAIcon,
+    items: [
+      ['Automated testing', automatedTestingIcon],
+      ['Manual testing', manualTestingIcon],
+      ['Performance testing', performanceTestingIcon],
+    ],
+  },
+  {
+    name: 'Consulting',
+    moreLink: 'https://www.itekako.com/consulting',
+    icon: consultingIcon,
+    items: [
+      ['Project specification development', projectSpecDevIcon],
+      ['Digital transformation', digitalTransformationIcon],
+      ['Technical due diligence', technicalDueDiligenceIcon],
+    ],
+  },
+  {
+    name: 'Building digital products end-to-end',
+    moreLink: 'https://www.itekako.com/building-digital-products-end-to-end',
+    icon: buildingDigitalProductsIcon,
+    items: [
+      ['Conceptualization & Prototyping', conceptualizationPrototypingIcon],
+      ['Product & Solution design', productSolutionDesignIcon],
+      ['MVP development', mvpDevelopmentIcon],
+      ['Software development', softwareDevelopmentIcon],
+      ['Software deployment', softwareDeploymentIcon],
+      ['Maintenance', maintenanceIcon],
+    ],
+  },
+]
+
+export const INDUSTRIES = [
+    {
+        name: 'Fintech',
+        icon: (<svg width="100%" height="100%" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" clipRule="evenodd" d="M22.5 0H30.0003V30H22.5V0ZM0 10.5588H15.0006V29.9999H0V10.5588Z" fill="black"></path>
+            </svg>) 
+    },
+    { 
+        name: 'Healthcare',
+        icon: (<svg width="100%" height="100%" viewBox="0 0 31 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" clipRule="evenodd" d="M11.25 0H18.7503V11.2504H11.2594V18.7496H18.75V11.2505H30.0094V18.7507H18.7503V30.0001H11.25V18.7496H0V11.2494H11.25V0Z" fill="black"></path>
+            </svg>),
+    },
+    {
+        name: 'Manufacturing',
+        icon: (<svg width="100%" height="100%" viewBox="0 0 34 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" clipRule="evenodd" d="M11.25 0H22.5004V11.2504H11.25V0ZM11.2504 18.7496H0V30.0001H11.2504V18.7496ZM33.7504 18.7496H22.5V30.0001H33.7504V18.7496Z" fill="black"></path>
+            </svg>)
+    },
+    {
+        name: 'Technology',
+        icon: (<svg width="100%" height="100%" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" clipRule="evenodd" d="M9.45605 9.74443C8.45172 10.6789 7.10521 11.2504 5.62521 11.2504C2.51849 11.2504 0 8.73193 0 5.62521C0 2.51849 2.51849 0 5.62521 0C8.73193 0 11.2504 2.51849 11.2504 5.62521C11.2504 7.10521 10.6789 8.45172 9.74443 9.45605H20.2556C19.3211 8.45172 18.7496 7.10521 18.7496 5.62521C18.7496 2.51849 21.2681 0 24.3748 0C27.4815 0 30 2.51849 30 5.62521C30 8.73193 27.4815 11.2504 24.3748 11.2504C22.8948 11.2504 21.5483 10.6789 20.544 9.74443V20.2556C21.5483 19.3211 22.8948 18.7496 24.3748 18.7496C27.4815 18.7496 30 21.2681 30 24.3748C30 27.4815 27.4815 30 24.3748 30C21.2681 30 18.7496 27.4815 18.7496 24.3748C18.7496 22.8948 19.3211 21.5483 20.2556 20.544H9.74443C10.6789 21.5483 11.2504 22.8948 11.2504 24.3748C11.2504 27.4815 8.73193 30 5.62521 30C2.51849 30 0 27.4815 0 24.3748C0 21.2681 2.51849 18.7496 5.62521 18.7496C7.10521 18.7496 8.45172 19.3211 9.45605 20.2556V9.74443Z" fill="black"></path>
+            </svg>)
+    },
+    { 
+        name: 'eCommerce',
+        icon: (<svg width="100%" height="100%" viewBox="0 0 39 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" clipRule="evenodd" d="M0 0H9.74618V4.26722H0V0ZM9.74618 4.26722L38.8764 4.26721L34.7657 17.2177H9.74609L9.74618 4.26722ZM34.7184 25.1086C34.7184 27.8111 32.5276 30.002 29.825 30.002C27.1225 30.002 24.9316 27.8111 24.9316 25.1086C24.9316 22.4061 27.1225 20.2152 29.825 20.2152C32.5276 20.2152 34.7184 22.4061 34.7184 25.1086ZM14.6403 25.1086C14.6403 27.8111 12.4494 30.002 9.7469 30.002C7.04436 30.002 4.85352 27.8111 4.85352 25.1086C4.85352 22.4061 7.04436 20.2152 9.7469 20.2152C12.4494 20.2152 14.6403 22.4061 14.6403 25.1086Z" fill="black"></path>
+            </svg>)
+    },
+    {
+        name: 'Automotive',
+        icon: (<svg width="100%" height="100%" viewBox="0 0 59 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" clipRule="evenodd" d="M44.1753 15.6816L31.7743 0H16.8422C12.5124 0 9 3.50962 9 7.84219C9 12.1719 12.5096 15.6844 16.8422 15.6844H44.1782L44.1753 15.6816ZM14.3213 22.8421C14.3213 26.7969 11.1153 30.0028 7.16063 30.0028C3.20592 30.0028 0 26.7969 0 22.8421C0 18.8874 3.20592 15.6815 7.16063 15.6815C11.1153 15.6815 14.3213 18.8874 14.3213 22.8421ZM58.7861 22.8421C58.7861 26.7969 55.5802 30.0028 51.6255 30.0028C47.6708 30.0028 44.4649 26.7969 44.4648 22.8422H44.1738V15.7268H50.8156C51.0815 15.6969 51.3517 15.6815 51.6255 15.6815C55.5802 15.6815 58.7861 18.8874 58.7861 22.8421Z" fill="black"></path>
+            </svg>)
+    }
+]
+
+export const WORK_MODES = [
+  {
+    title: 'Team augmentation',
+    copy:
+      'You get the best technical talents with zero hiring hassle. We can extend your technical teams quickly with our smartest tech talents who will operate as a part of your in-house team and adapt to your business culture and routines to ensure successful project delivery.',
+    benefits: [
+      'Fast ramp-up of your technical team',
+      'Easy rightsizing',
+      'Expert services and knowledge transfer',
+      'Keeps your full-time staffing lean',
+    ],
+    icon: team,
+  },
+  {
+    title: 'Dedicated teams',
+    copy:
+      'Our dedicated team of skilled technology experts will help you build digital products end-to-end on time, on scope, and in line with the highest standards of quality.',
+    benefits: ['Best tech talents available in the long run', 'Lower maintenance costs', 'You manage the team but we guarantee delivery'],
+    icon: partnership,
+  },
+  {
+    title: 'Project based',
+    copy:
+      'You own the product vision and we take full responsibility for every stage of product development - from ideation, design, to development and delivery. We optimize time, scope and cost along with team performance.',
+    benefits: ['No management overhead', 'Lower cost in the long run', 'Shared risk', 'You manage the team but we guarantee delivery'],
+    icon: project,
+  },
+]
+
+export const PROCESS = [
+  {
+    title: 'Discovery',
+    copy:
+      "Every project starts with a roundtable with key stakeholders to understand your company's vision and business goals. We analyze value propositions to form the right team of experts that will bring your solutions to life.",
+  },
+  {
+    title: 'Advisory',
+    copy:
+      "Whether you are planning to automate business processes or improve performance, we help you define the right business strategy to build human-centric solutions that will exceed your customers' expectations.",
+  },
+  {
+    title: 'Design',
+    copy:
+      'Leveraging the synergy of our creativity and deep engineering capabilities, our multidisciplinary teams work closely with you to design the right game-changing products.',
+  },
+  {
+    title: 'Dev & QA',
+    copy:
+      'Implementing all the necessary insights and requirements gathered in the previous phases, we deliver disruptive, intuitive and captivating solutions that drive your digital transformation.',
+  },
+  {
+    title: 'Support',
+    copy:
+      'We support your enterprise with scalable solutions every step of the way by providing maintenance plans and enhancing your ability to stay agile and scale.',
+  },
+]
+
